ee409e65795be960b2b82e6df0296752b345a437
Laurence Spiller <lspiller@cern.ch>
2015-12-18 08:52:51 +0000

    small commit

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
 from setuptools import setup
 from Cython.Build import cythonize
-setup(ext_modules=cythonize('moments.pyx', annotate=True))
+#setup(ext_modules=cythonize('moments.pyx', annotate=True))
+setup(name='moments', version='1.0.0',  ext_modules=cythonize('moments.pyx', annotate=True, line_directives=True))
